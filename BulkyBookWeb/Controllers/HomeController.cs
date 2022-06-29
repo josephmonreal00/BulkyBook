c312e48579bd9c3790b094cdde9094f74b03f47e
Joseph_Monreal <Joseph_Monreal@uhaul.com>
2022-06-29 11:16:14 -0700

    Add project files.

---
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyBookWeb.Controllers
+{
+    // Controller
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Action
+        // ex - https://localhost:55555/home/index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Action
+        // ex - https://localhost:55555/home/privacy
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
